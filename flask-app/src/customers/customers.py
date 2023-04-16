03c835a04315db8a69055531d74c808a4c1c9b95
Zachary Rippas <83041437+zatchet@users.noreply.github.com>
2023-04-16 18:51:59 -0400

    unfinished customer routes

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -5,33 +5,65 @@ from src import db
 
 customers = Blueprint('customers', __name__)
 
-# Get all customers from the DB
-@customers.route('/customers', methods=['GET'])
-def get_customers():
-    cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
-        first_name, job_title, business_phone from customers')
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+# register yourself as a new customer
+@customers.route('/', methods=['POST'])
+def signup():
+    data = request.json
 
-# Get customer detail for customer with particular userID
-@customers.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
+    first = data['first']
+    last = data['last']
+    phone1 = data['phone1']
+    try:
+        phone2 = data['phone2']
+    except:
+        phone2 = None
+    card = data['card']
+    cvv = data['cvv']
+    expiration = data['expiration']
+    street = data['street']
+    city = data['city']
+    state = data['state']
+    zip_code = data['zip_code']
+    
+    query = f'''
+    insert into customers
+    (first_name, last_name, phone1, phone2, card_num, cvv, expiration_date, street, city, state, zip_code)
+    values ({first}, {last}, {phone1}, {phone2}, {card}, {cvv}, {expiration}, {street}, {city}, {state}, {zip_code})
+    '''
+
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return f'Successfully registered customer {first} {last}!'
+
+# place an order
+@customers.route('/orders/<customerID>', methods=['POST'])
+def place_order(customerID):
+    data = request.json()
+    item_ids = data['items']
+    employee = data['employee_id']
+    location = data['location_id']
+    restrictions = data['restrictions']
+
+    query1 = f'''
+    insert into orders
+    (location_id, customer_id, employee_id, status, restrictions)
+    values({location}, {customerID}, {employee}, 0, {restrictions})
+    '''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query1)
+
+    query2= 'insert into orderitems (item_id, order_id, item_quantity)'
+
+    
+
+    for item in item_ids:
+        query2 += f'values({item}, {cursor.lastrowid}, {qty})'
+    
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
