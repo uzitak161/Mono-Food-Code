e70186042af4a648e03dd05aca234ad4b9da288b
liamsmeraglia <liamsmeraglia1225@gmail.com>
2023-04-16 20:50:58 -0400

    manager routes

--- flask-app/src/managers/managers.py
+++ flask-app/src/managers/managers.py
@@ -51,7 +51,7 @@ def get_revenue_for_location(locationID):
     where location_id = {ID}
     group by Orders.order_id) as OrderTotals
     """
-    query = query.format(ID = locationID)
+    query = query.format(ID=locationID)
 
     cursor = db.get_db().cursor()
     cursor.execute(query)
@@ -65,3 +65,136 @@ def get_revenue_for_location(locationID):
     the_response.mimetype = 'application/json'
     return the_response
 
+
+@managers.route('/customers', methods=['GET'])
+def get_customers():
+    query = """
+    select customer_id, first_name, last_name, phone1, phone2,\
+     street, city, state, zip_code from Customers
+    """
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@managers.route('/orders/location/<string:locationID>', methods=['GET'])
+def get_orders_for_location(locationID):
+    start_date = request.args.get('startDate')
+    end_date = request.args.get('endDate')
+    query = """
+    select order_id, customer_id, employee_id, status,\
+     order_date, restrictions from Orders
+    where location_id = {ID}
+    """
+    if start_date and end_date:
+        query += f"and order_date BETWEEN '{start_date}' AND '{end_date}'"
+    query = query.format(ID=locationID)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@managers.route('/orders/employee/<string:employeeID>', methods=['GET'])
+def get_orders_for_employee(employeeID):
+    start_date = request.args.get('startDate')
+    end_date = request.args.get('endDate')
+    query = """
+    select order_id, customer_id, employee_id, status,\
+     order_date, restrictions from Orders
+    where employee_id = {ID}
+    """
+    if start_date and end_date:
+        query += f"and order_date BETWEEN '{start_date}' AND '{end_date}'"
+    query = query.format(ID=employeeID)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+@managers.route('/items', method=['POST'])
+def add_menu_item():
+    data = request.get_json()
+
+    if not data or 'item_name' not in data or 'item_price' not in data \
+            or 'ingredients' not in data or 'availability' not in data:
+        return make_response(jsonify({'error': 'Invalid request body'}), 400)
+
+    item_name = data['item_name']
+    item_price = data['item_price']
+    ingredients = data['ingredients']
+    availability = data['availability']
+
+    query = """
+    INSERT INTO Items (item_name, item_price, ingredients, availability)
+    VALUES ('{name}', {price}, '{ingredients}', {availability})
+    """.format(name=item_name, price=item_price, ingredients=ingredients, availability=availability)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return make_response(jsonify({'message': 'Item added'}), 201)
+
+
+@managers.route('/items/<string:itemID>', method=['PUT'])
+def update_menu_item(itemID):
+    data = request.get_json()
+
+    if not data or 'item_name' not in data or 'item_price' not in data \
+            or 'ingredients' not in data or 'availability' not in data:
+        return make_response(jsonify({'error': 'Invalid request body'}), 400)
+
+    item_name = data['item_name']
+    item_price = data['item_price']
+    ingredients = data['ingredients']
+    availability = data['availability']
+
+    query = """
+    UPDATE Items
+    SET item_name = '{name}', item_price = {price}, ingredients = '{ingredients}', availability = {availability}
+    WHERE item_id = {ID}
+    """.format(name=item_name, price=item_price, ingredients=ingredients, availability=availability, ID=itemID)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return make_response(jsonify({'message': 'Item updated'}), 200)
+
+
+@managers.route('/items/<string:itemID>', method=['DELETE'])
+def delete_menu_item(itemID):
+    query = """
+    DELETE FROM Items
+    WHERE item_id = {ID}
+    """.format(ID=itemID)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return make_response(jsonify({'message': 'Item deleted'}), 200)
