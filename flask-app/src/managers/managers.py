a0018c6578095b357b246a0c8afeb14d22e9d0e8
liamsmeraglia <liamsmeraglia1225@gmail.com>
2023-04-19 13:21:46 -0400

    added orders by item route and tested

--- flask-app/src/managers/managers.py
+++ flask-app/src/managers/managers.py
@@ -350,6 +350,26 @@ def update_menu_item(itemID):
 
     return make_response(jsonify({'message': 'Item updated'}), 200)
 
+@managers.route('/items/<string:itemID>/orders', methods=['GET'])
+def num_orders_for_item(itemID):
+    query = """
+    SELECT SUM(item_quantity) FROM Orders JOIN OrderItems ON Orders.order_id = OrderItems.order_id
+    WHERE item_id = {ID}
+    """
+    query = query.format(ID=itemID)
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
 @managers.route('/schedule', methods=['PUT'])
 def update_schedule():
     data = request.get_json()
