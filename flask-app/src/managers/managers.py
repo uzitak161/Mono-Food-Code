1a39e1b41ccfea2e3fa038eb4139fe812da2b99c
liamsmeraglia <liamsmeraglia1225@gmail.com>
2023-04-19 17:12:54 -0400

    get all items for manager

--- flask-app/src/managers/managers.py
+++ flask-app/src/managers/managers.py
@@ -286,6 +286,23 @@ def get_orders_for_employee(employeeID):
     the_response.mimetype = 'application/json'
     return the_response
 
+@managers.route('/items', methods=['GET'])
+def get_all_menu_items():
+    query = """
+    select item_id, item_name, item_price, ingredients, availability from Items
+    """
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
 
 @managers.route('/items', methods=['POST'])
 def add_menu_item():
